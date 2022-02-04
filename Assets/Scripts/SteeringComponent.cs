85af289db025c0c92b0c202b0faf3e7965903ceb
ogike <tasi.zoltan07@gmail.com>
2022-02-04 16:47:25 +0100

    init
    Need to do the individual components!!!!

--- Assets/Scripts/SteeringComponent.cs
+++ Assets/Scripts/SteeringComponent.cs
@@ -12,11 +12,13 @@ public abstract class SteeringComponent : MonoBehaviour
 
     protected Vector3 curPos;
     protected Vector3 steeringDir;
+    public float steeringMag;
 
     public float steeringWeight = 1;
 
     //TODO: centralize debugging
     public float debugLineLength = 1; //0 if no debug
+    public float debugArrowSize = 0.5f;
     public Color debugLineColor = Color.blue;
 
     // Start is called before the first frame update
@@ -35,17 +37,35 @@ public abstract class SteeringComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        steeringDir = CalcSteeringDir();
-        myHandler.AddSteerDir(steeringDir, steeringWeight);
+        //steeringDir = CalcSteeringDir();
+        //myHandler.AddSteerDir(steeringDir, steeringWeight);
 
-        if (debugLineLength > 0)
-            DrawDebugLine();
+        //if (debugLineLength > 0)
+        //    DrawDebugLine();
     }
 
     public abstract Vector3 CalcSteeringDir();
 
-    public virtual void DrawDebugLine()
-	{
-        Debug.DrawLine(curPos, curPos + (steeringDir * debugLineLength), Color.blue, Time.deltaTime);
+    public virtual void DrawDebugLine(Vector3 dir, float weight, float arrowSize = 0f)
+    {
+        //curPos = myTrans.position;
+        //Debug.DrawLine(curPos, curPos + (dir * debugLineLength * weight), debugLineColor, Time.deltaTime);
+
+        Vector3 endPoint = myTrans.position + (dir * weight * debugLineLength);
+
+        Debug.DrawLine(myTrans.position, endPoint, debugLineColor, Time.deltaTime);
+
+        if (arrowSize > 0 && dir.magnitude > 0.1f)
+        {
+            float arrowLength = 0.5f * arrowSize;
+            float arrowWideness = 0.33f * arrowSize;
+            Vector3 arrowBackDir = (myTrans.position - endPoint).normalized;
+            Vector3 arrowLeftDir = Vector3.Cross(arrowBackDir, Vector3.forward).normalized;
+            Vector3 arrowTmpPoint = endPoint + (arrowBackDir * arrowLength);
+
+            Debug.DrawLine(endPoint, arrowTmpPoint + (arrowLeftDir * arrowWideness), debugLineColor, Time.deltaTime);
+            Debug.DrawLine(endPoint, arrowTmpPoint + (-arrowLeftDir * arrowWideness), debugLineColor, Time.deltaTime);
+
+        }
     }
 }
